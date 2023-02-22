4422f5f0b2bcc3e54ab94b65d3f772587c372e55
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-22 19:36:49 +0530

    Search Questions

--- Algorithms/src/Searching_Algo/LinearSearch/SearchIn2D.java
+++ Algorithms/src/Searching_Algo/LinearSearch/SearchIn2D.java
@@ -1,4 +1,24 @@
 package Searching_Algo.LinearSearch;
 
 public class SearchIn2D {
+    public static void main(String[] args) {
+        int[][] arr ={
+                {2,4,6},
+                {6,8,4},
+                {-8,1,4}
+        };
+        System.out.println(findMin(arr));
+    }
+    static int findMin(int[][] arr){
+        int min = Integer.MAX_VALUE;
+        for(int row =0;row<arr.length;row++){
+            for(int column=0;column<arr[row].length;column++){
+                if(arr[row][column] < min){
+                    min=arr[row][column];
+                }
+            }
+
+        }
+        return min;
+    }
 }
