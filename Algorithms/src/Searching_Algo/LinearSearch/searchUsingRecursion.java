a5e58ae362ed72285f0f40efcf77facb055a6fd4
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-28 21:09:47 +0530

    linear search using recursion

--- Algorithms/src/Searching_Algo/LinearSearch/searchUsingRecursion.java
+++ Algorithms/src/Searching_Algo/LinearSearch/searchUsingRecursion.java
@@ -1,10 +1,14 @@
 package Searching_Algo.LinearSearch;
 
+import java.util.ArrayList;
+
 public class searchUsingRecursion {
     public static void main(String[] args) {
-        int[] arr = {3,6,8,9,10};
+        int[] arr = {3,6,8,9,9,10};
         int target = 9;
         System.out.println( search(arr,target,0));
+        search1(arr,target,0);
+        System.out.println(search2(arr,target,0,new ArrayList<>()));
     }
     static void search1(int[] arr,int target,int index){
         if(index==arr.length) {
@@ -13,9 +17,8 @@ public class searchUsingRecursion {
         }
         if(arr[index]==target){
             System.out.println("Found");
-            return;
         }else{
-            search(arr,target,index+1);
+            search1(arr,target,index+1);
         }
     }
     static boolean search(int[] arr,int target,int index) {
@@ -23,5 +26,13 @@ public class searchUsingRecursion {
             return false;
         }
         return arr[index] == target || search(arr, target, index + 1);
+    }
+    static ArrayList<Integer> search2(int[] arr, int target, int index,ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+        if(arr[index]==target){
+            list.add(index);
+        } return search2(arr, target, index+1, list);
     }
 }
