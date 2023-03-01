b7b091cbad9160dd58c1a3d24af1600ac1549637
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-03-01 20:55:19 +0530

    patterns using recursion

---
+++ Algorithms/src/Recursion/PractiseQuestions/paternsUsingRec.java
@@ -0,0 +1,19 @@
+package Recursion.PractiseQuestions;
+
+public class paternsUsingRec {
+    public static void main(String[] args) {
+        pattern(5,0);
+    }
+    static void pattern(int row,int col){
+        if(row==0){
+            return;
+        }
+        if(col<row){
+            System.out.print("*");
+            pattern(row,col+1);
+        }else{
+            System.out.println();
+            pattern(row-1,0);
+        }
+    }
+}
--- Collection Framework/src/Main.java
+++ Collection Framework/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int size = sc.nextInt();
+            int[] arr1= new int[size];
+            int[] arr2= new int[size];
+            int count = 0;
+            for(int i = 0;i<size;i++){
+                arr1[i]=sc.nextInt();
+            }
+            for(int j = 0;j<size;j++){
+                arr2[j]=sc.nextInt();
+            }
+            for(int l = 1;l<size;l++){
+                if(arr1[l]-arr1[l-1] <= arr2[l]){
+                    count++;
+                }
+            }System.out.println(count+1);
+        }
     }
 }
