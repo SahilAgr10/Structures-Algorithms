ee7be068f67fdce476f6c3f3d06c7616ff0ca6d6
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-03-01 21:02:35 +0530

    patterns using recursion

--- Algorithms/src/Recursion/PractiseQuestions/paternsUsingRec.java
+++ Algorithms/src/Recursion/PractiseQuestions/paternsUsingRec.java
@@ -2,18 +2,37 @@ package Recursion.PractiseQuestions;
 
 public class paternsUsingRec {
     public static void main(String[] args) {
-        pattern(5,0);
+        pattern(5,0);//triangle rev pattern
+        System.out.println();
+        pattern2(5,0);// triangle straight pattern
     }
     static void pattern(int row,int col){
         if(row==0){
             return;
         }
         if(col<row){
-            System.out.print("*");
             pattern(row,col+1);
+            System.out.print("*");
+
+        }else{
+            pattern(row-1,0);
+            System.out.println();
+
+        }
+    }
+
+    static void pattern2(int row,int col){
+        if(row==0){
+            return;
+        }
+        if(col<row){
+            System.out.print("*");
+            pattern2(row,col+1);
         }else{
             System.out.println();
-            pattern(row-1,0);
+            pattern2(row-1,0);
+
+
         }
     }
 }
