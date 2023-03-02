924a49bd5585c7c07062cbac04f0309c82b86261
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-03-02 23:48:42 +0530

    patterns using recursion

---
+++ Algorithms/src/Sorting_Algo/SelectionSort/SsortUsingRec.java
@@ -0,0 +1,16 @@
+package Sorting_Algo.SelectionSort;
+
+public class SsortUsingRec {
+    public static void main(String[] args) {
+        int[] arr = {3,1,5,7};
+        sort(arr,4,0);
+    }
+    static void sort(int[] arr,int row ,int col){
+         if(row==0){
+             return;
+         }
+         if(col<row-1){
+
+         }
+    }
+}
