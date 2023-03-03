09d3a11f3eb0ebadcebbb691aae2001cd805ca80
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-03-03 11:31:19 +0530

    selection sort using recursion

--- Algorithms/src/Sorting_Algo/SelectionSort/SsortUsingRec.java
+++ Algorithms/src/Sorting_Algo/SelectionSort/SsortUsingRec.java
@@ -1,17 +1,28 @@
 package Sorting_Algo.SelectionSort;
 
+import java.util.Arrays;
+
 public class SsortUsingRec {
     public static void main(String[] args) {
         int[] arr = {3,1,5,7};
-        sort(arr,4,0);
+        sort(arr,4,0,0);
+        System.out.println(Arrays.toString(arr));
     }
-    static void sort(int[] arr,int row ,int col){
+    static void sort(int[] arr,int row ,int col,int max){
          if(row==0){
              return;
          }
-         if(col<row-1){
-             //TODO
-
+         if(col<row){
+             if(arr[col]>arr[max]){
+                 sort(arr, row, col+1, col);
+             } else{
+                 sort(arr, row, col+1, max);
+             }
+         }else{
+             int temp = arr[max];
+             arr[max]= arr[row-1];
+             arr[row-1] = temp;
+             sort(arr, row-1, 0, 0);
          }
     }
 }
