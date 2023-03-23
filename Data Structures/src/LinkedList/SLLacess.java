f0c7b5b657701b3e92c93d3fe1d7412a70b3e0ee
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-03-23 22:14:05 +0530

    LinkedList implementation

--- Data Structures/src/LinkedList/SLLacess.java
+++ Data Structures/src/LinkedList/SLLacess.java
@@ -1,4 +1,9 @@
 package LinkedList;
 
 public class SLLacess {
+    public static void main(String[] args) {
+        SLL list = new SLL();
+        list.insertFirst(6);
+        list.insertLast(8);
+    }
 }
